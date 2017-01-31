69eb17c93491be79d851d42e6b7800c01d65f62c
hyunjae <kimhun456@gmail.com>
2017-01-31 19:46:32 +0900

    test practice for node js

---
+++ app/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const converter = require("./converter");
+
+
+app.get("/rgbToHex", function(req,res){
+    var red   = parseInt(req.query.red, 10);
+    var green = parseInt(req.query.green, 10);
+    var blue  = parseInt(req.query.blue, 10);
+
+    var hex = converter.rgbToHex(red, green, blue);
+
+    res.send(hex);
+});
+app.get("/hexToRgb", function(req,res){
+    var hex = req.query.hex;
+    var rgb = converter.hexToRgb(hex);
+
+    res.send(JSON.stringify(rgb));
+});
+
+app.listen(3000, function(){
+    console.log("localhost server is running at 3000");
+});
+
